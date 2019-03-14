39904714ba8b05ceb7d366487dbd07f44a8ce9ca
Scott Ian <sim2487@gmail.com>
2019-03-14 03:36:32 -0700

    add demo creds

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link, Redirect} from 'react-router-dom';
-import {login} from '../actions/auth';
+// import {login} from '../actions/auth';
 import './landingPage.css';
-import { showFindGame } from '../actions/sideBar-actions';
+// import { showFindGame } from '../actions/sideBar-actions';
 import LoginForm from './loginForm';
 
 export function LandingPage(props) {
@@ -12,21 +12,21 @@ export function LandingPage(props) {
         return <Redirect to="/dashboard" />;
     }
     
-    const onSubmit = () =>  {
-        props.dispatch(login('spectator', '0123456789'));
-        props.dispatch(showFindGame())
-    }
+    // const onSubmit = () =>  {
+    //     props.dispatch(login('spectator', '0123456789'));
+    //     props.dispatch(showFindGame())
+    // }
     
     return (
         <div className="home">
             <h1>Gametron</h1>
             <p>Use this app to easily create or join games.</p>
             <p>Compete against other players to achieve Vicory!</p>
-            <p>
+            {/* <p>
                 Just want to spectate? <span 
                 className='submit-link' 
                 onClick={()=>onSubmit()}>Click Here</span>  
-            </p>
+            </p> */}
             
             <p>
                 <Link className='link' to="/register">Create a New Account</Link> or login below.
@@ -34,6 +34,13 @@ export function LandingPage(props) {
             <div className='logIn subcard'>
                 <LoginForm />
             </div>
+            <br />
+            <p>
+            Just want to demo the app? Use these:
+            <br /> Username: demo 
+            <br /> Password: password12
+            </p>
+            
         </div>
     );
 }
